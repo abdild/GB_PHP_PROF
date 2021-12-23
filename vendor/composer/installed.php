4265c2df6b79f74fe5690eb08ca10748ee02b950
unknown <abdullin@yandex.ru>
2021-12-23 16:33:07 +0300

    lesson3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '956b332580bdb619c4c4a86e7e899ef9adaf9541',
+        'name' => 'vendor_name/phpprof',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'twig/twig' => array(
+            'pretty_version' => '1.6.1',
+            'version' => '1.6.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twig/twig',
+            'aliases' => array(),
+            'reference' => '30929fde86f5ee12c863a3b572b0457ce52c140b',
+            'dev_requirement' => false,
+        ),
+        'vendor_name/phpprof' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '956b332580bdb619c4c4a86e7e899ef9adaf9541',
+            'dev_requirement' => false,
+        ),
+    ),
+);
